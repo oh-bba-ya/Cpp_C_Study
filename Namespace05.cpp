ca73664bc75b10679589a15a272dbaca8325af13
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2019-07-08 20:00:15 +0900

    Update Namespace05.cpp

--- Namespace05.cpp
+++ Namespace05.cpp
@@ -21,3 +21,20 @@ int main(void)
 //Hello World!
 //Hello World!
 //20 A 3.14
+
+
+#include <iostream>
+using namespace std;			//'이름공간 std에 선언된 모든것에 대해 이름공간 지정의 생략' 명령
+
+int main(void)
+{
+	int num = 20;
+	cout << "Hello World!" << endl;
+	cout << "Hello " << "World!" << endl;
+	cout << num << ' ' << 'A';
+	cout << ' ' << 3.14 << endl;
+	return 0;
+}
+
+//만일 using을 일일히 std에 선언된 모든것에 대해 이름공간 지정의 생략을 원한다면 즉,std 이름공간안에 있는 선언된 것들을 일일히 타이핑하기 귀찮을때
+//27행 :'이름공간 std에 선언된 모든것에 대해 이름공간 지정의 생략' 위의 첫번째 예제처럼 일일이 타이핑하기 귀찮을
