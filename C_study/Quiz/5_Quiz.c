5faaf5a2e1f26096d4cb060d585bb659c4430c35
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2019-08-14 13:26:46 +0900

    Update 5_Quiz.c

--- C_study/Quiz/5_Quiz.c
+++ C_study/Quiz/5_Quiz.c
@@ -40,3 +40,51 @@ int main(void)
 		printf("5의 배수입니다.\n");
 	}
 }
+
+
+//문제3
+//이번학기 점수를 입력받아 성적을 나타내시오.
+//100~90:A, 89~80:B, 79~70:C, 69~60:D 59~0:F
+#include <stdio.h>
+
+int main(void)
+{
+	int score = 0;
+	printf("점수를 입력하시오:");
+	scanf_s("%d", &score);
+
+	if (score >= 90)
+	{
+		printf("A\n");
+	}
+	else
+	{
+		if (score >= 80)
+		{
+			printf("B\n");
+		}
+		
+		else
+		{
+			if (score >= 70)
+			{
+				printf("C\n");
+			}
+
+			else
+			{
+				if (score >= 60)
+				{
+					printf("D\n");
+				}
+
+				else
+				{
+					printf("F\n");
+				}
+			}
+		}
+	}
+
+	return 0;
+}
