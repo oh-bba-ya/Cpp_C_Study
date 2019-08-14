695f0d87b1612b930a4ce24596b3a152b7e24e42
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2019-08-14 13:08:37 +0900

    Update 5_Quiz.c

--- C_study/Quiz/5_Quiz.c
+++ C_study/Quiz/5_Quiz.c
@@ -15,3 +15,28 @@ int main(void)
 	printf("x의 값은:%d", x);
 }
 
+//문제2
+//x의 값을 입력받아 2의 배수인지 3의배수인지 5의배수인지 알아내는 프로그램을 작성하시오.
+#include <stdio.h>
+
+int main(void)
+{
+	int x;
+
+	printf("x의 값을 입력하시오: ");
+	scanf_s("%d", &x);
+	if (x % 2==0)
+	{
+		printf("2의배수입니다.\n");
+	}
+	
+	if (x % 3 == 0)
+	{
+		printf("3의 배수입니다.\n");
+	}
+
+	if (x % 5 == 0)
+	{
+		printf("5의 배수입니다.\n");
+	}
+}
