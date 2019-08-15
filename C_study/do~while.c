0e060a28fb8909f6d462a05e57e4562b726edd7b
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2019-08-15 23:16:07 +0900

    Update do~while.c

--- C_study/do~while.c
+++ C_study/do~while.c
@@ -15,3 +15,23 @@ int main(void)
 	printf("1부터 10까지의 합은 %d입니다.", sum);
 	return 0;
 }
+
+
+//음의 정수가 입력되기 전까지 입력받은 정수의 합
+
+#include <stdio.h>
+
+int main(void)
+{
+	int x, sum=0;
+
+	do
+	{
+		printf("정수를 입력하세요: ");
+		scanf_s("%d", &x);
+		sum = sum + x;
+	} while (x > 0);
+
+	printf("입력한 정수의 합은 %d입니다.\n", sum);
+	return 0;
+}
