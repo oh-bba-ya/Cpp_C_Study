9d8f50f8c2ef5f3535aaa06503bcee33012b2fab
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2019-08-17 16:27:01 +0900

    Update 배열.c

--- C_study/배열.c
+++ C_study/배열.c
@@ -48,3 +48,20 @@ int main(void)
 
 	return 0;
 }
+
+
+//배열의 크기.
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 0, x[] = { 0,1,2,3,4 };
+	
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d", x[i]);
+	}
+
+	printf("\n배열의 크기 = %d", sizeof(x));			//크기가 4바이트인 int형 원소 5개가 있으므로 출력결과:20.
+	printf("\n배열의 크기 = %d", sizeof(x) / sizeof(int));	//x의 원소크기가 20바이트 이므로 int형 (4byte)로 나누게 되면 5.
+}									//즉,전체 바이트를 int형 크기로 나누게 되면 x 배열의 크기를 알 수 있다.
