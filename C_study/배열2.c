74ee92b32a61475d9f324009f0f764737447e927
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2019-08-17 17:41:19 +0900

    Update 배열2.c

--- C_study/배열2.c
+++ C_study/배열2.c
@@ -9,3 +9,17 @@ int main(void)
 	for (i = 0; i < 5; i++) printf("%.1f\n", x[i]);
 	return 0;
 }
+
+
+//다차원배열.
+#include <stdio.h>
+
+int main(void)
+{
+	int a[2][3] = { {10,20,50},{20,30,40} };		//{국어,영어,수학}.
+	int i = 0, j = 0;
+	for (i = 0; i < 2; i++)
+		for (j = 0; j < 3; j++)
+			printf("a[%d][%d] = %d\n", i, j, a[i][j]);
+	return 0;
+}
