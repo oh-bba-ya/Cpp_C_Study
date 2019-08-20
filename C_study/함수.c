401283a48ffd0bed20f2ebd3ff5603d13762dbd4
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2019-08-20 20:43:36 +0900

    Update 함수.c

--- C_study/함수.c
+++ C_study/함수.c
@@ -30,3 +30,33 @@ int main()
 //add start: x=3, y=4, z=-1
 //add end: x=3, y=4, z=7
 //main end: c=7
+
+//함수호출과정2
+#include <stdio.h>
+
+int add(int x, int y)
+{
+	int z = -1;
+	printf("add start: x=%d, y=%d, z=%d\n", x, y, z);
+
+	z = x + y;
+
+	printf("add end: x=%d, y=%d, z=%d\n", x, y, z);
+	return z;
+}
+
+int main()
+{
+	int a = 4, b = 3;
+	int v1, v2, v3, v4, sum;
+
+	v1 = add(a, a + b);
+	v2 = add(1, a + 2);
+	v3 = add(1 + 2, a) - 3;
+	sum = add(1, b) + add(a, 2);
+	v4 = add(a, add(1, 2));
+
+	printf("v1=%d , v2=%d, v3=%d, sum = %d,v4=%d", v1, v2, v3, sum, v4);
+
+	return 0;
+}
