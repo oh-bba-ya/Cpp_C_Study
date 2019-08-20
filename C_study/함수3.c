972dff8bc77f576544cd0fc7d09d7c53a788536d
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2019-08-20 21:17:42 +0900

    Update 함수3.c

--- C_study/함수3.c
+++ C_study/함수3.c
@@ -14,3 +14,20 @@ int main(void)
 	print(score);
 	return 0;
 }
+
+//배열의 크기가 변경되는 경우에 대비 하기 위하여 배열의 크기를 인자로 넣을 수 있다.
+#include <stdio.h>
+
+void print(int ar[ ], int size)
+{
+	int i;
+	for (i = 0; i < size ; i++) printf("%d", ar[i]);
+	printf("\n");
+}
+
+int main(void)
+{
+	int score[3] = { 70, 80, 90 };
+	print(score,3);
+	return 0;
+}
