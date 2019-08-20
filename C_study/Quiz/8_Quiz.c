739909541987148f74b6545ff910996638ad6fda
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2019-08-20 20:20:52 +0900

    Update 8_Quiz.c

--- C_study/Quiz/8_Quiz.c
+++ C_study/Quiz/8_Quiz.c
@@ -1,7 +1,27 @@
-//문제
+//문제1
 //max함수와 min함수를 작성하시오.
 //int형 변수a,b.
 //int 반환
+#include <stdio.h>
+
+int MAX(int a, int b)
+{
+	int c;
+	c = a > b ? a : b;
+
+	return c;
+}
+
+int MIN(int a, int b)
+{
+	int c;
+	c = a < b ? a : b;
+	return c;
+}
+
+
+//문제2
+//divive함수를 만드시오
 #include <stdio.h>
 
 int MAX(int a, int b)
