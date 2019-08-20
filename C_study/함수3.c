d4f02b4a18375476b94b01ea8b5df049fca5fe4f
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2019-08-20 21:15:02 +0900

    Update 함수3.c

--- C_study/함수3.c
+++ C_study/함수3.c
@@ -1,7 +1,7 @@
  //함수에 배열 전체를 전달하는 경우 함수 실인자로써 배열이름만 쓴다.
  #include <stdio.h>
 
-void print(int ar[3])                   //void print(int ar[]) 배열의 크기를 생략해도 된다.
+void print(int ar[3])                   //void print(int ar[]) 배열의 크기 생략해도 된다.
 {
 	int i;
 	for (i = 0; i < 3; i++) printf("%d", ar[i]);
