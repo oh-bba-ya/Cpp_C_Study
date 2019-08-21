84faf0d9d39d03abc2cf12d94b67e1082f6f8ce4
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2019-08-21 18:16:03 +0900

    Update 문자열.c

--- C_study/문자열.c
+++ C_study/문자열.c
@@ -24,3 +24,18 @@ int main(void)
 
 	return 0;
 }
+
+
+//문자열 "A"와 문자배열'A'는 다르다.
+//문자열은 \0을 마지막에 포함시키므로 크기가 다르기에 배열크기를 문자열로 할시엔 문자배열의 크기에 +1해줘야한다.
+#include <stdio.h>
+
+int main(void)
+{
+	char str1[] = { 'H','e','l','l','o' };		//문자 배열.
+	char str2[] = "Hello";						//문자열.
+
+	printf("%d %d", sizeof(str1), sizeof(str2));		//결과 출력:5 6.
+
+	return 0;
+}
