45fbf20ce746ab78ca874894832d321fee553e5e
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2019-08-26 20:41:29 +0900

    Update and rename DefaultValue2.cpp to C++_study/Chapter1/DefaultValue2.cpp

---
+++ C++_study/Chapter1/DefaultValue2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+int Adder(int num1 = 1, int num2 = 2);   //함수의 선언 위치 여기에다가만 디폴트값을 설정해주어야한다.
+
+int main(void)
+{
+	std::cout << Adder() << std::endl;
+	std::cout << Adder(5) << std::endl;
+	std::cout << Adder(3, 5) << std::endl;
+	return 0;
+}
+
+int Adder(int num1, int num2)
+{
+	return num1 + num2;
+}
+
+
+//매개변수의 디폴트 값 지정은 함수의 선언부분에 위치한다.
+//함수의 선언이 별도로 필요한 경우에는 매개변수의 디폴트 값은 함수의 선언부분에만 위치시켜야한다.
