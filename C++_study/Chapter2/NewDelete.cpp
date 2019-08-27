9f77213b1dc5e688fa41c0d2ec287ec74fd60fdb
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2019-08-27 21:30:23 +0900

    Update and rename NewDelete.cpp to C++_study/Chapter2/NewDelete.cpp

--- C++_study/Chapter2/NewDelete.cpp
+++ C++_study/Chapter2/NewDelete.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 char * MakeStrAdr(int len)
 {
-	// char * str= (char*)malloc(sizeof(char)*len);						//7,8행: 단순비교를 하더라도 new를 이용한 동적 할당이 훨씬 간결함을 알 수 있다.
+	// char * str= (char*)malloc(sizeof(char)*len);		//7,8행: 단순비교를 하더라도 new를 이용한 동적 할당이 훨씬 간결함을 알 수 있다.
 	char * str = new char[len];
 	return str;
 }
@@ -15,7 +15,7 @@ int main(void)
 	strcpy(str, "I am so happy~");
 	cout << str << endl;
 	//free(str);
-	delete[]str;									//18행:배열의 형태로 할당된 메모리 공간의 해제를 보이고 있다.
+	delete[]str;			//18행:배열의 형태로 할당된 메모리 공간의 해제를 보이고 있다.
 	return 0;
 }
 
